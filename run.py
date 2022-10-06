2a53e50963aea5033a922db7a1ab86d2bd9feffe
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-10-06 18:23:02 +0200

    quitamos la pantalla emergente

--- run.py
+++ run.py
@@ -3,8 +3,4 @@ import sys
 import menu
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "-t":
-        menu.iniciar()
-    else:
-        app = ui.MainWindow()
-        app.mainloop()
+    menu.iniciar()
